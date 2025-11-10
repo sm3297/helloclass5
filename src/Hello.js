76118a4a5f44dd1485b286443ad2436beb3c2e9f
Seongmin <holykim707@gmail.com>
2025-11-10 13:39:16 +0900

    test

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import './Hello.css';
+
+function Hello() {
+  return (
+    <div className="Hello">
+      <header className="Hello-header">
+        <h1>Welcome to the Hello Component!</h1>
+        <p>
+          This is a simple React component.
+        </p>
+      </header>
+    </div>
+  );
+}
